6f4de76fe27bf8653df68272aa10f3eda0ec891a
pyr33x <pyrexrj@gmail.com>
2025-07-24 00:35:59 +0330

    feat: define conn packet struct

--- internal/packet/packet.go
+++ internal/packet/packet.go
@@ -3,13 +3,19 @@ package packet
 type Type byte
 
 const (
-	CONNECT   Type = 1
-	CONNACK   Type = 2
-	PUBLISH   Type = 3
-	SUBSCRIBE Type = 8
+	CONNECT Type = 0x10
 )
 
 type Packet struct {
 	Type Type
 	Raw  []byte
+}
+
+type ConnectPacket struct {
+	ProtocolName  string
+	ProtocolLevel byte
+	CleanSession  bool
+	KeepAlive     uint16
+	ClientID      string
+	Raw           []byte
 }
