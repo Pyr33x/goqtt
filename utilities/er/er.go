4380a26b6dfdf61abeda5d727c107c84cd06e1ad
pyr33x <pyrexrj@gmail.com>
2025-07-24 00:36:22 +0330

    feat: update errors

--- utilities/er/er.go
+++ utilities/er/er.go
@@ -11,15 +11,19 @@ type Err struct {
 }
 
 var (
-	ErrEmptyBuffer       = errors.New("Buffer is empty")
-	ErrReadBuffer        = errors.New("Could not read buffer")
-	ErrShortBuffer       = errors.New("Buffer is too short for string length")
-	ErrReadProtoName     = errors.New("Failed to read protocol name")
-	ErrMissProtoVer      = errors.New("Missing protocol version")
-	ErrMissConnectFlags  = errors.New("Missing connect flags")
-	ErrMissKeepAlive     = errors.New("Missing Keep Alive")
-	ErrReadClientID      = errors.New("Failed to read client ID")
-	ErrInvalidConnPacket = errors.New("Connect packet is invalid")
+	ErrEmptyBuffer           = errors.New("Buffer is empty")
+	ErrReadBuffer            = errors.New("Could not read buffer")
+	ErrShortBuffer           = errors.New("Buffer is too short for string length")
+	ErrReadProtoName         = errors.New("Failed to read protocol name")
+	ErrMissProtoVer          = errors.New("Missing protocol version")
+	ErrMissProtoLevel        = errors.New("Missing protocol level")
+	ErrMissConnectFlags      = errors.New("Missing connect flags")
+	ErrMissKeepAlive         = errors.New("Missing Keep Alive")
+	ErrReadClientID          = errors.New("Failed to read client ID")
+	ErrInvalidConnPacket     = errors.New("Connect packet is invalid")
+	ErrInvalidPacketType     = errors.New("Packet type is invalid")
+	ErrRemainingLenMissmatch = errors.New("Remaining length mismatch")
+	ErrShortString           = errors.New("String is too short")
 )
 
 func (e *Err) Error() string {
