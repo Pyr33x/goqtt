5e0682cc6c6667d7104f52309ed433e33e0c9555
pyr33x <pyrexrj@gmail.com>
2025-07-25 14:23:52 +0330

    feat: update erros + implement unwrap

--- pkg/er/er.go
+++ pkg/er/er.go
@@ -24,8 +24,22 @@ var (
 	ErrInvalidPacketType     = errors.New("packet type is invalid")
 	ErrRemainingLenMissmatch = errors.New("remaining length mismatch")
 	ErrShortString           = errors.New("string is too short")
+	ErrIdentifierRejected    = errors.New("identifier rejected")
+
+	// ClientID
+	ErrEmptyClientID                = errors.New("empty client id requires clean session to be 1")
+	ErrEmptyAndCleanSessionClientID = errors.New("client id is empty and clean session is set to 0")
+	ErrClientIDLengthExceed         = errors.New("client id exceeds 23 bytes")
+	ErrInvalidCharsClientID         = errors.New("client id contains invaid characters")
+
+	// Client
+	ErrClientMustSetCleanSession = errors.New("client must set clean session to 1")
 )
 
 func (e *Err) Error() string {
 	return fmt.Sprintf("context: %s, message: %v", e.Context, e.Message)
+}
+
+func (e *Err) Unwrap() error {
+	return e.Message
 }
