10cf2f15c08d2fe4df901ef6cd519ba6e36d5727
pyr33x <pyrexrj@gmail.com>
2025-07-25 16:13:31 +0330

    fix(err): remove unused errors

--- pkg/er/er.go
+++ pkg/er/er.go
@@ -11,36 +11,18 @@ type Err struct {
 }
 
 var (
-	ErrEmptyBuffer           = errors.New("buffer is empty")
-	ErrReadBuffer            = errors.New("could not read buffer")
-	ErrShortBuffer           = errors.New("buffer is too short for string length")
-	ErrReadProtoName         = errors.New("failed to read protocol name")
-	ErrMissProtoVer          = errors.New("missing protocol version")
-	ErrMissProtoLevel        = errors.New("missing protocol level")
-	ErrMissConnFlags         = errors.New("missing connect flags")
-	ErrMissKeepAlive         = errors.New("missing Keep Alive")
-	ErrReadClientID          = errors.New("failed to read client ID")
-	ErrInvalidConnPacket     = errors.New("connect packet is invalid")
-	ErrInvalidPacketType     = errors.New("packet type is invalid")
-	ErrRemainingLenMissmatch = errors.New("remaining length mismatch")
-	ErrShortString           = errors.New("string is too short")
-	ErrIdentifierRejected    = errors.New("identifier rejected")
-
-	// ClientID
+	ErrShortBuffer                  = errors.New("buffer is too short")
+	ErrInvalidConnPacket            = errors.New("connect packet is invalid")
+	ErrInvalidPacketType            = errors.New("packet type is invalid")
+	ErrIdentifierRejected           = errors.New("identifier rejected")
 	ErrEmptyClientID                = errors.New("empty client id requires clean session to be 1")
 	ErrEmptyAndCleanSessionClientID = errors.New("client id is empty and clean session is set to 0")
 	ErrClientIDLengthExceed         = errors.New("client id exceeds 23 bytes")
-	ErrInvalidCharsClientID         = errors.New("client id contains invaid characters")
-
-	// Protocol
-	ErrUnsupportedProtocolLevel = errors.New("protocol level is not supported")
-	ErrUnsupportedProtocolName  = errors.New("protocol name is not supported")
-
-	// ConnectFlags
-	ErrInvalidWillQos = errors.New("willqos level is invalid")
-
-	// Client
-	ErrClientMustSetCleanSession = errors.New("client must set clean session to 1")
+	ErrInvalidCharsClientID         = errors.New("client id contains invalid characters")
+	ErrUnsupportedProtocolLevel     = errors.New("protocol level is not supported")
+	ErrUnsupportedProtocolName      = errors.New("protocol name is not supported")
+	ErrInvalidWillQos               = errors.New("willqos level is invalid")
+	ErrPasswordWithoutUsername      = errors.New("password flag set without username flag")
 )
 
 func (e *Err) Error() string {
