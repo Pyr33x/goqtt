9dc864ebee501d9455a3a1c2d732540c47f1b66a
pyr33x <pyrexrj@gmail.com>
2025-07-27 16:43:28 +0330

    hotfix(connack): typo

--- internal/packet/connack.go
+++ internal/packet/connack.go
@@ -1,7 +1,7 @@
 package packet
 
 const (
-	ConnectionAccepted          = 0x00 // Connection Accpeted
+	ConnectionAccepted          = 0x00 // Connection Accepted
 	UnacceptableProtocolVersion = 0x01 // The Server does not support the level of the MQTT protocol requested by the Client
 	IdentifierRejected          = 0x02 // The Client identifier is correct UTF-8 but not allowed by the Server
 	ServerUnavailable           = 0x03 // The Network Connection has been made but the MQTT service is unavailable
