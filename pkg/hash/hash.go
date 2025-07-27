7a8084692545b45ac48a562323db142042c59319
pyr33x <pyrexrj@gmail.com>
2025-07-28 01:00:49 +0330

    fix(hash): remove unused func

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,21 +1,8 @@
 package hash
 
 import (
-	"github.com/pyr33x/goqtt/pkg/er"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func HashPasswd(passwd string, cost int) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(passwd), cost)
-	if err != nil {
-		return "", &er.Err{
-			Context: "Hash",
-			Message: er.ErrHashFailed,
-		}
-	}
-
-	return string(hash), nil
-}
 
 func VerifyPasswd(hash, passwd string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(passwd))
