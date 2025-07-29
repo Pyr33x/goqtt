a1cc989952af5f3b66dc3a128d4b6bb7143885a8
pyr33x <pyrexrj@gmail.com>
2025-07-29 23:49:22 +0330

    feat(packet): add unsub to parsed packet struct

--- internal/packet/packet.go
+++ internal/packet/packet.go
@@ -21,12 +21,13 @@ const (
 )
 
 type ParsedPacket struct {
-	Type       PacketType
-	Raw        []byte
-	Connect    *ConnectPacket
-	Publish    *PublishPacket
-	Subscribe  *SubscribePacket
-	Disconnect *DisconnectPacket
+	Type        PacketType
+	Raw         []byte
+	Connect     *ConnectPacket
+	Publish     *PublishPacket
+	Subscribe   *SubscribePacket
+	Unsubscribe *UnsubscribePacket
+	Disconnect  *DisconnectPacket
 }
 
 // IsConnect returns true if this is a CONNECT packet
